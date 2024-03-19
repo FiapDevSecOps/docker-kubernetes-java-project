31801f99b1af223b08d0c42aea9d48e013216ce5
Rosthan Silva <rosthanlinux@gmail.com>
2024-03-19 00:32:52 -0300

    all done

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,88 +1,100 @@
-variable "region" {
-  default = "eu-central-1"
+provider "aws" {
+  region = "us-east-1"
 }
 
-data "aws_availability_zones" "available" {}
+terraform {
+  required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.6.0"
+    }
+  }
 
-locals {
-  cluster_name = "JAVA-EKS-Cluster"
+  required_version = "~> 1.0"
 }
-
 
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.14.3"
 
-  name = "JAVA-EKS-VPC"
+  name = "main"
   cidr = "10.0.0.0/16"
 
-  azs             = data.aws_availability_zones.available.names
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.0.0/19", "10.0.32.0/19"]
+  public_subnets  = ["10.0.64.0/19", "10.0.96.0/19"]
 
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  one_nat_gateway_per_az = false
 
   enable_dns_hostnames = true
+  enable_dns_support   = true
+
   tags = {
-    "Name" = "JAVA-EKS-VPC"
-  }
-  public_subnet_tags = {
-    "Name" = "EKS-Public-Subnet"
-  }
-  rivate_subnet_tags = {
-    "Name" = "EKS-Private-Subnet"
+    Environment = "staging"
   }
 }
 
 
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "17.18.0"
-  cluster_name    = local.cluster_name
-  cluster_version = "1.23"
-  subnets         = module.vpc.private_subnets
+  source  = "terraform-aws-modules/eks/aws"
+  version = "18.29.0"
+
+  cluster_name    = "my-eks"
+  cluster_version = "1.28"
+
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  enable_irsa = true
+
+  eks_managed_node_group_defaults = {
+    disk_size = 50
+  }
+
+  eks_managed_node_groups = {
+    general = {
+      desired_size = 1
+      min_size     = 1
+      max_size     = 10
+
+      labels = {
+        role = "general"
+      }
+
+      instance_types = ["t3.small"]
+      capacity_type  = "ON_DEMAND"
+    }
+
+    spot = {
+      desired_size = 4
+      min_size     = 2
+      max_size     = 10
+
+      labels = {
+        role = "spot"
+      }
+
+      taints = [{
+        key    = "market"
+        value  = "spot"
+        effect = "NO_SCHEDULE"
+      }]
+
+      instance_types = ["t3.micro"]
+      capacity_type  = "SPOT"
+    }
+  }
+
   tags = {
-    Name = "JAVA-EKS-Cluster"
+    Environment = "staging"
   }
-  vpc_id = module.vpc.vpc_id
-  workers_group_defaults = {
-    root_volume_type = "gp3"
-  }
-  worker_groups = [
-    {
-      name                          = "Worker-Group-1"
-      instance_type                 = "t2.medium"
-      asg_desired_capacity          = 2
-      additional_security_group_ids = [aws_security_group.worker_group_one.id]
-    },
-    {
-      name                          = "Worker-Group-2"
-      instance_type                 = "t2.medium"
-      asg_desired_capacity          = 1
-      additional_security_group_ids = [aws_security_group.worker_group_two.id]
-    },
-  ]
-}
-
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
-}
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
-}
-
-
-provider "kubernetes" {
-
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  cluster_ca_certificate = base64encode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-}
-
-
-output "cluster_id" {
-  value = module.eks.cluster_id
-}
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
 }
